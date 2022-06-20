4d18ebf88e4a0abb91c8d2e2c2a571727d24f3a6
Xavier Bodart <bodxav@gmail.com>
2022-06-20 11:03:38 +0200

    Defining Enum QuestionType

--- src/main/java/trivia/domain/QuestionType.java
+++ src/main/java/trivia/domain/QuestionType.java
@@ -1,4 +1,10 @@
 package trivia.domain;
 
 public enum QuestionType {
+    POP("Pop"),SCIENCE("Science"),SPORTS("Sports"),ROCK("Rock"),GEOGRAPHY("Geography");
+
+    public final String label;
+    QuestionType(String label) {
+        this.label = label;
+    }
 }
