3fb5caa07541d8659e7095ff71fe456e2f9e28f5
Xavier Bodart <bodxav@gmail.com>
2022-06-20 11:18:25 +0200

    Defining Player

--- src/main/java/trivia/domain/Player.java
+++ src/main/java/trivia/domain/Player.java
@@ -1,4 +1,20 @@
 package trivia.domain;
 
-public class Player {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Player implements IPlayer{
+    private String name;
+    private int nbCoins;
+    private boolean isInPenaltyBox;
+    //private int goodAnswersStreak;
+    private Cell currentCell;
+    public Player(String name, Cell currentCell) {
+        this.name = name;
+        this.currentCell = currentCell;
+    }
 }
